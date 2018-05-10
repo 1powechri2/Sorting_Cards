1e2dbe49b16254da3a5b33095bee7f23677d1fc1
Chris Powell <chris.ian.powell@gmail.com>
2018-05-10 06:27:06 -0600

    deleted merge sort attempt iteration 3 complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Deck
   attr_reader :cards
 
@@ -23,25 +22,6 @@ class Deck
       end
       @counter +=1
       sort
-    end
-  end
-
-  def merge_sort
-    if @cards_sorted.length == @cards.length
-      @cards_sorted
-    else
-    half = @cards.length/2
-    left = @cards.take(half)
-    right = @cards.drop(half)
-    binding.pry
-      if left.first.value <= right.first.value
-        @cards_sorted.push(left.first)
-        left.shift
-      else
-        @cards_sorted.push(right.first)
-        right.shift
-      end
-      merge_sort
     end
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,5 +1,4 @@
 require './test/test_helper'
-require 'pry'
 
 class DeckTest < Minitest::Test
   def setup
@@ -19,15 +18,12 @@ class DeckTest < Minitest::Test
   def test_cards_passed_into_deck
     assert_equal [@card_1, @card_2, @card_3], @deck.cards
   end
+
   def test_deck_count
     assert_equal 3, @deck.count
   end
 
   def test_sort
     assert_equal [@card_2, @card_1, @card_3], @deck.sort
-  end
-
-  def test_merge_sort
-    assert_equal [@card_2, @card_1, @card_3], @deck.merge_sort
   end
 end
